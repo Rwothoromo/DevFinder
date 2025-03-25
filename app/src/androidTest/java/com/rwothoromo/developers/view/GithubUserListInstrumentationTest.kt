cb29478862e8e618c4db747b5d80be734b010d09
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2025-03-25 11:58:52 +0300

    Adjust wait time on Instrumented tests

--- app/src/androidTest/java/com/rwothoromo/developers/view/GithubUserListInstrumentationTest.kt
+++ app/src/androidTest/java/com/rwothoromo/developers/view/GithubUserListInstrumentationTest.kt
@@ -38,7 +38,7 @@ import org.junit.runner.RunWith
 class GithubUserListInstrumentationTest {
     private val context: Context = ApplicationProvider.getApplicationContext()
 
-    val intent = Intent(context, MainActivity::class.java)
+    private lateinit var intent: Intent
 
     /**
      * Very important for launching activity and passing tests
@@ -50,36 +50,70 @@ class GithubUserListInstrumentationTest {
      */
     @Before
     fun setUp() {
+        intent = Intent(context, MainActivity::class.java)
         activityScenario = launchActivity<MainActivity>(intent)
         IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
         // Using TestUtils.executeWithDelay below to skip the alertDialog in MainActivity
     }
 
-//    /**
-//     * Click Refresh in the menu items.
-//     */
-//    @Test
-//    fun clickActionBarOverflowRefresh() {
-//        // Wait 5 seconds for alertDialog to close
-//        TestUtils.executeWithDelay {
-//            // Open the options menu OR open the overflow menu, depending on whether
-//            // the device has a hardware or software overflow menu button.
-//            openActionBarOverflowOrOptionsMenu(context)
-//
-//            // Click the item.
-//            onView(withText(R.string.action_refresh)).perform(click())
-//        }
-//    }
+    /**
+     * Click the Search icon on the Action bar.
+     */
+    @Test
+    fun clickActionBarSearchItem() {
+        // Wait 6 seconds for alertDialog to close
+        TestUtils.executeWithDelay(6000L) {
+            // Open the options menu OR open the overflow menu, depending on whether
+            // the device has a hardware or software overflow menu button.
+            openActionBarOverflowOrOptionsMenu(context)
+
+            // Click on the Search icon.
+            onView(withId(R.id.action_search)).perform(click())
+        }
+    }
+
+    /**
+     * Click Settings in the menu items.
+     */
+    @Test
+    fun clickActionBarOverflowSettings() {
+        // Wait 7 seconds for alertDialog to close
+        TestUtils.executeWithDelay(7000L) {
+            // Open the options menu OR open the overflow menu, depending on whether
+            // the device has a hardware or software overflow menu button.
+            openActionBarOverflowOrOptionsMenu(context)
+
+            // Click the item.
+            onView(withText(R.string.action_settings)).perform(click())
+        }
+    }
+
+    /**
+     * Click Refresh in the menu items.
+     */
+    @Test
+    fun clickActionBarOverflowRefresh() {
+        // Wait 8 seconds for alertDialog to close
+        TestUtils.executeWithDelay(8000L) {
+            // Open the options menu OR open the overflow menu, depending on whether
+            // the device has a hardware or software overflow menu button.
+            openActionBarOverflowOrOptionsMenu(context)
+
+            // Click the item.
+            onView(withText(R.string.action_refresh)).perform(click())
+        }
+    }
 
     /**
      * Scroll to a GitHub user on the RecyclerView and click one.
      */
     @Test
     fun clickableRecyclerViewItems() {
-        // Wait 5 seconds for data to load and alertDialog to close
-        TestUtils.executeWithDelay {
+        // Wait 9 seconds for data to load and alertDialog to close
+        TestUtils.executeWithDelay(9000L) {
             // Confirm toolbar is displayed
             onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+            onView(withText(R.string.app_name)).check(matches(withParent(withId(R.id.toolbar))))
             onView(withText(R.string.app_name)).check(matches(withParent(withId(R.id.toolbar))))
 
             // Confirm recyclerview is displayed
